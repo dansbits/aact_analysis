7a589573c2639b80f0aedaf802fae093ebfeeed7
dan <daniel.carpenter01@gmail.com>
2016-07-05 02:46:52 -0700

    Update initializer

--- lib/initializer.rb
+++ lib/initializer.rb
@@ -1,6 +1,10 @@
 require 'sequel'
+require 'yaml'
 require_relative 'aact/schema_creator'
 require_relative 'aact/text_file_processor'
+require_relative 'aact/date_transformer'
+
+db_config = db_config = YAML.load_file("database.yml")
 
 # set up the database connection
-DB = Sequel.connect(adapter: 'sqlite', database: 'aact.db')
+DB = Sequel.connect(db_config)
