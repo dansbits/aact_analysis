54fd4b471452a4ae11e8b24826c294662758b29b
dan <daniel.carpenter01@gmail.com>
2016-07-24 14:58:03 -0700

    Setup initializer to use serets file

--- lib/initializer.rb
+++ lib/initializer.rb
@@ -1,10 +1,19 @@
 require 'sequel'
 require 'yaml'
+require 'mysql'
 require_relative 'aact/schema_creator'
 require_relative 'aact/text_file_processor'
 require_relative 'aact/date_transformer'
 
-db_config = db_config = YAML.load_file("database.yml")
+db_config = db_config = YAML.load_file("secrets.yml")['database']
 
 # set up the database connection
-DB = Sequel.connect(db_config)
+sequel_config = {
+  adapter: 'mysql',
+  database: db_config['name'],
+  user: db_config['user'],
+  password: db_config['password'],
+  host: db_config['host']
+}
+
+DB = Sequel.connect(sequel_config)
